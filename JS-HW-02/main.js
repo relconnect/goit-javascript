9e5cf3b5bc3a0456ba24391e96047a2222257984
Vitaliy Bohdan <vitaliy.flashnika@gmail.com>
2018-09-05 22:03:56 +0300

    add new script

--- JS-HW-02/main.js
+++ JS-HW-02/main.js
@@ -1,22 +1,42 @@
-let arr = [];
-let number;
-let summ = null;
-do {
-  number = prompt("Enter a number");
-  if (number === null) break;
-  isNumber = Number(number);
-  if (!Number.isNaN(isNumber)) {
-    arr.push(isNumber);
-  } else {
-    break;
+let userInput;
+const numbers = [];
+let total = 0;
+do{
+  userInput = prompt('Enter a number', 0);
+  if (!Number.isNaN(+userInput)) {
+    numbers.push(+userInput);
+  }else {
+    alert("It's not a number! Please try again!");   
   }
-} while (number != "");
+} while(userInput !== null);
+if (numbers.length !== 0) {
+    for (let val of numbers) {
+      total += val;
+    }
+    alert(`Общая сумма чисел равна: ${total}`);
+  } 
 
-if (arr.length) {
-  for (let val of arr) {
-    summ += val;
-  }
-  alert(`Общая сумма чисел равна: ${summ}`);
-} else {
-  alert("Массив пустой!");
-}
+// let arr = [];
+// let number;
+// let summ = null;
+// do {
+//   number = prompt("Enter a number");
+//   if (number === null) break;
+//   isNumber = Number(number);
+//   if (!Number.isNaN(isNumber)) {
+//     arr.push(isNumber);
+//   } else {
+//     break;
+//   }
+// } while (number != "");
+
+// if (arr.length) {
+//   for (let val of arr) {
+//     summ += val;
+//   }
+//   alert(`Общая сумма чисел равна: ${summ}`);
+// } else {
+//   alert("Массив пустой!");
+// }
+
+
