0ea519ffcb8b058febcd0b89b012ad12835f3658
Vitaliy Bohdan <vitaliy.flashnika@gmail.com>
2018-09-26 16:28:54 +0300

    add additional

---
+++ JS-HW-05/main.js
@@ -0,0 +1,82 @@
+//                                       Доп задание №1
+/*  Перепишите все циклы for используя методы map, filter, find, reduce и т. д. */
+
+/* 
+  Функция findGreaterThan получает два аргумента - число и массив.
+  Возвращает новый массив, содержащий элементы которые больше числа.
+*/
+// const findGreaterThan = (num, arr) => {
+//     const result = [];
+  
+//     for (let i = 0, max = arr.length; i < max; i += 1) {
+//       if (arr[i] > num) {
+//         result.push(arr[i]);
+//       }
+//     }
+  
+//     return result;
+//   };
+
+  const findGreaterThan = (num, arr) => arr.filter(x => x >num);
+  
+  
+  console.log( findGreaterThan(2, [1, 2, 3, 4, 5]) ); // [3, 4, 5,]
+  console.log( findGreaterThan(3, [1, 2, 3, 4, 5]) ); // [4, 5,]
+  console.log( findGreaterThan(1, [1, 2, 3, 4, 5]) ); // [2, 3, 4, 5,]
+  
+  /* 
+    Функция multiplyBy принимает два аргумента - число и массив. 
+    Возвращает массив все значения которого умножены на число.
+  */
+//   const multiplyBy = (num, arr) => {
+//     let result = [];
+  
+//     for (let i = 0, max = arr.length; i < max; i += 1) {
+//       result.push(arr[i] * num);
+//     }
+  
+//     return result;
+//   };
+ const multiplyBy = (num, arr) => arr.map(x => x*num);
+  
+  console.log( multiplyBy(2, [1, 2, 3, 4, 5]) ); // [2, 4, 6, 8, 10]
+  console.log( multiplyBy(3, [1, 2, 3, 4, 5]) ); // [3, 6, 9, 12, 15]
+  console.log( multiplyBy(4, [1, 2, 3, 4, 5]) ); // [4, 8, 12, 16, 20]
+  
+  /* 
+    Функция summAllNumbers принимает любое число аргументов.
+    Возвращает число - сумму всех аргументов.
+  */
+//   function summAllNumbers(...args) {
+//     let accumulator = 0;
+  
+//     for (let i = 0, max = args.length; i < max; i += 1) {
+//       accumulator += args[i];
+//     }
+  
+//     return accumulator;
+//   }
+summAllNumbers = (...args) => args.reduce((accum, elem) => accum + elem, 0);
+  
+  console.log( summAllNumbers(1, 2, 3) ); // 6
+  console.log( summAllNumbers(1, 2, 3, 4) ); // 10
+  console.log( summAllNumbers(1, 2, 3, 4, 5) ); // 15
+  
+  /* 
+    Функция findEvery получает два аргумента - число и массив.
+    Возвращает true если все элементы массива больше или равны числу.
+    Иначе если есть хоть один элемент меньше числа, то возвращается false.
+  */
+//   const findEvery = (num, arr) => {
+//     for (let i = 0, max = arr.length; i < max; i += 1) {
+//       if (arr[i] < num) {
+//         return false;
+//       }
+//     }
+  
+//     return true;
+//   };
+  const findEvery = (num, arr) => arr.every(x => x >= num);
+  console.log( findEvery(5, [5, 6, 7, 8, 9]) ); // true
+  console.log( findEvery(6, [5, 6, 7, 8, 9]) ); // false
+  console.log( findEvery(4, [5, 6, 7, 8, 9]) ); // true
