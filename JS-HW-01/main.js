f0d759c545189f9ea6ce37a3bd3bc964f52b3378
Vitaliy Bohdan <vitaliy.flashnika@gmail.com>
2018-10-10 11:17:27 +0300

    add hw-06

--- JS-HW-01/main.js
+++ JS-HW-01/main.js
@@ -3,21 +3,21 @@ const adminPassword = "m4ngo1zh4ackz0r";
 let userLogin;
 let userPassword;
 
-// userLogin = prompt("Введите Login");
-// if (userLogin === null) {
-//   alert("Отменено пользователем!");
-// } else if (userLogin != adminLogin) {
-//   alert("Доступ запрещен!");
-// } else {
-//   userPassword = prompt("Введите пароль");
-//   if (userPassword === null) {
-//     alert("Отменено пользователем!");
-//   } else if (userPassword != adminPassword) {
-//     alert("Доступ запрещен!");
-//   } else {
-//     alert("Добро пожаловать!");
-//   }
-// }
+userLogin = prompt("Введите Login");
+if (userLogin === null) {
+  alert("Отменено пользователем!");
+} else if (userLogin != adminLogin) {
+  alert("Доступ запрещен!");
+} else {
+  userPassword = prompt("Введите пароль");
+  if (userPassword === null) {
+    alert("Отменено пользователем!");
+  } else if (userPassword != adminPassword) {
+    alert("Доступ запрещен!");
+  } else {
+    alert("Добро пожаловать!");
+  }
+}
 
 // const min = 1;
 // const max = 100;
---
+++ JS-HW-06/src/script.js
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3];
+
+
+ // Функциональный forEach
+ numbers.forEach(num => console.log(num*2));
+ console.log(numbers);
