8ea13e897c3707b161eafaaf271ccd159700613f
Vitaliy Bohdan <vitaliy.flashnika@gmail.com>
2018-11-16 14:16:45 +0200

    add hw-11

--- JS-HW-11/main.js
+++ JS-HW-11/main.js
@@ -9,7 +9,7 @@ const laptops = [
     price: 28000,
     release_date: 2015,
     name: 'Macbook Air White 13"',
-    img: "http://demo.posthemes.com/pos_zadademo/images/placeholder.png",
+    img: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
     descr:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae."
   },
@@ -19,7 +19,7 @@ const laptops = [
     price: 32000,
     release_date: 2016,
     name: 'Macbook Air Gray 13"',
-    img: "http://demo.posthemes.com/pos_zadademo/images/placeholder.png",
+    img: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
     descr:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae."
   },
@@ -29,7 +29,7 @@ const laptops = [
     price: 35000,
     release_date: 2017,
     name: 'Macbook Air Black 13"',
-    img: "http://demo.posthemes.com/pos_zadademo/images/placeholder.png",
+    img: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
     descr:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae."
   },
@@ -39,7 +39,7 @@ const laptops = [
     price: 45000,
     release_date: 2015,
     name: 'Macbook Air White 15"',
-    img: "http://demo.posthemes.com/pos_zadademo/images/placeholder.png",
+    img: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
     descr:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae."
   },
@@ -49,7 +49,7 @@ const laptops = [
     price: 55000,
     release_date: 2016,
     name: 'Macbook Pro Gray 15"',
-    img: "http://demo.posthemes.com/pos_zadademo/images/placeholder.png",
+    img: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
     descr:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae."
   },
@@ -59,7 +59,7 @@ const laptops = [
     price: 45000,
     release_date: 2017,
     name: 'Macbook Pro Black 15"',
-    img: "http://demo.posthemes.com/pos_zadademo/images/placeholder.png",
+    img: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
     descr:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae."
   },
@@ -69,7 +69,7 @@ const laptops = [
     price: 65000,
     release_date: 2015,
     name: 'Macbook Air White 17"',
-    img: "http://demo.posthemes.com/pos_zadademo/images/placeholder.png",
+    img: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
     descr:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae."
   },
@@ -79,7 +79,7 @@ const laptops = [
     price: 75000,
     release_date: 2016,
     name: 'Macbook Pro Gray 17"',
-    img: "http://demo.posthemes.com/pos_zadademo/images/placeholder.png",
+    img: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
     descr:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae."
   },
@@ -89,23 +89,47 @@ const laptops = [
     price: 80000,
     release_date: 2017,
     name: 'Macbook Pro Black 17"',
-    img: "http://demo.posthemes.com/pos_zadademo/images/placeholder.png",
+    img: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05962448.png",
     descr:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae."
   }
 ];
 
 let btn = document.querySelector(".submit");
+let reset = document.querySelector(".reset");
 
-let createFilteredCards = ({ size, color, release_date }) => {
-  let filters = [];
-  filters = filters.concat(size, color, release_date);
-  let filteredCards = laptops.filter(elem =>
-    Object.values(elem).some(el => filters.includes(el))
-  );
+// let createFilteredCards = ({ size, color, release_date }) => {
+//   let filters = [];
+//   filters = filters.concat(size, color, release_date);
+//   let filteredCards = laptops.filter(elem =>
+//     Object.values(elem).some(el => filters.includes(el))
+//   );  
 
+//   render(filteredCards);
+// };
+
+
+let createFilteredCards = (obj) => {
+  let filters = obj;
+  let filteredCards = laptops.filter(elem => {
+    let includeColor = true;
+    let includeSize = true;
+    let includeDate =true;
+ 
+    if(filters.color.length != 0){
+      includeColor = filters.color.includes(elem.color);
+    };
+    if(filters.size.length != 0){
+      includeSize = filters.size.includes(elem.size);
+    };
+    if(filters.release_date.length != 0){
+      includeDate = filters.release_date.includes(elem.release_date);
+    };
+    return includeColor && includeSize && includeDate;
+  })  
   render(filteredCards);
 };
+
 
 let render = (arr) => {
   const markup = arr.reduce((acc, item) => acc + template(item), '');
@@ -118,7 +142,9 @@ btn.addEventListener("click", e => {
   const filter = { size: [], color: [], release_date: [] };
   const nameArray = document.querySelectorAll('input[type="checkbox"]:checked');
 
-  Array.from(nameArray).forEach(elem => {
+  let checkedList = Array.from(nameArray);
+  
+  checkedList.forEach(elem => {
     if (elem.name == "size") {
       filter.size.push(+elem.value);
     } else if (elem.name == "color") {
@@ -129,4 +155,14 @@ btn.addEventListener("click", e => {
   });
 
   createFilteredCards(filter);
+});
+
+reset.addEventListener("click",e =>{
+  let arr = [...laptops]; 
+  render(arr);
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+  let arr = [...laptops]; 
+  render(arr);
 });
---
+++ JS-HW-12/build/js/scripts.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var a = function a(x, b) {
+  return x * b;
+};
+a(4, 6);
